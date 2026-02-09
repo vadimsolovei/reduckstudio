9aaa8e079d75b2949b5b6e8c9a464903966f1cf0
Vadim Solovei <vadim.solovei@softwaremind.com>
2026-02-09 19:13:39 +0200

    Simplify gradient background, fix font paths, and refine transitions
      - Replace SVG goo filter with CSS blur for gradient background
      - Remove mix-blend-mode and add will-change for gradient circles
      - Fix font path for 11ty build and rewrite in css:wp script for WP
      - Scope theme transitions to text elements instead of universal
        selector
      - Remove fixed hero height and CTA will-change
      - Simplify mobile gradient to single circle with reduced blur

--- wp-content/themes/reduck-theme/template-parts/components/gradient-bg.php
+++ wp-content/themes/reduck-theme/template-parts/components/gradient-bg.php
@@ -6,20 +6,6 @@
     </filter>
     <rect width="100%" height="100%" filter="url(#noiseFilterBg)" />
   </svg>
-  <svg xmlns="http://www.w3.org/2000/svg" class="svg-blur">
-    <defs>
-      <filter id="goo">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-        <feColorMatrix
-          in="blur"
-          mode="matrix"
-          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-          result="goo"
-        />
-        <feBlend in="SourceGraphic" in2="goo" />
-      </filter>
-    </defs>
-  </svg>
   <div class="gradients-container">
     <div class="g1"></div>
     <div class="g2"></div>
